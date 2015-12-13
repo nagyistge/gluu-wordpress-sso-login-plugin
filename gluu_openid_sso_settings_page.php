a33d7bc6921ae8e2087f255a4a67b15c2a73f221
unknown <vlad.karapetyan.1988@mail.ru>
2015-12-14 00:14:23 +0400

    adding new oxd openId registration page

--- gluu_openid_sso_settings_page.php
+++ gluu_openid_sso_settings_page.php
@@ -59,6 +59,68 @@ function oxd_register_openid() {
 		<?php
 
 }
+function oxd_openid_show_new_registration_page() {
+	update_option ( 'oxd_openid_new_registration', 'true' );
+	global $current_user;
+		get_currentuserinfo();
+	?>
+
+					<form name="f" method="post" action="" id="register-form">
+								<input type="hidden" name="option" value="oxd_openid_connect_register_customer" />
+
+
+
+								<div class="oxd_openid_table_layout">
+									<?php if(!is_oxd_registered()) { ?>
+										<div style="font-weight:bold ;display:block;margin-top:10px;color:red;background-color:rgba(251, 232, 0, 0.15);padding:5px;border:solid 1px rgba(255, 0, 9, 0.36);">
+                                            Please enter gluu and oxd configuration for continue.
+                                        </div>
+									<?php } ?>
+
+										<h3>Enter gluu and oxd configuration.</h3>
+
+										<p>Please enter a valid email that you have access to. Login using your gluu credentials.
+										</p>
+										<table class="oxd_openid_settings_table">
+											<tr>
+												<td><b><font color="#FF0000">*</font>Email:</b></td>
+												<td><input class="oxd_openid_table_textbox" type="email" name="email"
+													required placeholder="person@example.com"
+													value="<?php echo $current_user->user_email;?>" /></td>
+											</tr>
+											<tr>
+												<td><b><font color="#FF0000">*</font>Gluu server url:</b></td>
+												<td><input class="oxd_openid_table_textbox" required type="url"
+													name="gluu_server_url" placeholder="Enter your gluu server url." /></td>
+											</tr>
+											<tr>
+												<td><b><font color="#FF0000">*</font>Oxd host ip:</b></td>
+												<td><input class="oxd_openid_table_textbox" required type="text"
+													name="oxd_host_ip" value="127.0.0.1" placeholder="Enter your oxd host ip." /></td>
+											</tr>
+											<tr>
+												<td><b><font color="#FF0000">*</font>Oxd host port:</b></td>
+												<td><input class="oxd_openid_table_textbox" required type="number"
+													name="oxd_host_port" value="8099" placeholder="Enter your oxd host port (Min. number 0, Max. number 65535)" /></td>
+											</tr>
+											<tr>
+												<td>&nbsp;</td>
+												<td><br /><input type="submit" name="submit" value="Next" style="width:100px;"
+													class="button button-primary button-large" /></td>
+											</tr>
+										</table>
+
+								</div>
+		</form>
+				<script>
+						//jQuery("#phone").intlTelInput();
+						var text = "&nbsp;&nbsp;We will call only if you need support."
+						jQuery('.intl-number-input').append(text);
+
+				</script>
+		<?php
+}
+
 
 
 
