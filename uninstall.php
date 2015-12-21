775eb263345b3e1c69a6376c0c12bb6bd315d37b
unknown <vlad.karapetyan.1988@mail.ru>
2015-12-21 15:37:52 +0400

    adding linkedin and twitter,

--- uninstall.php
+++ uninstall.php
@@ -1,13 +1,16 @@
 <?php
 	//delete all stored key-value pairs
 	delete_option('oxd_config');
+	delete_option('oxd_id');
 	delete_option('oxd_openid_host_name');
 	delete_option('oxd_openid_new_registration');
 	delete_option('oxd_openid_admin_email');
 	delete_option('oxd_openid_message');
 	delete_option('oxd_openid_google_enable');
 	delete_option('oxd_openid_facebook_enable');
-	delete_option('oxd_openid_SMS_enable');
+	delete_option('oxd_openid_linkedin_enable');
+	delete_option('oxd_openid_twitter_enable');
+	delete_option('oxd_openid_Duo_enable');
 	delete_option('oxd_openid_U2F_enable');
 	delete_option('oxd_openid_default_login_enable');
 	delete_option('oxd_openid_default_register_enable');
@@ -15,12 +18,12 @@
 	delete_option('oxd_openid_woocommerce_login_form');
 	delete_option('oxd_openid_login_redirect');
 	delete_option('oxd_openid_login_redirect_url');
-	delete_option('oxdOpenId_social_login_avatar');
+	delete_option('oxdOpenId_gluu_login_avatar');
 	delete_option('oxdOpenId_user_attributes');
 
 	delete_option( 'oxd_openid_login_theme' );
 	delete_option( 'oxd_openid_login_button_customize_text');
-	delete_option('oxd_openid_Mobile_enable');
+	delete_option('oxd_openid_Basic_enable');
 
 	delete_option('oxd_login_icon_custom_size');
 	delete_option('oxd_login_icon_space' );
